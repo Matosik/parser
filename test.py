9e206515b98b27d426aec93913c37751809c3799
Matosik <boga4iov.matvei2016@yandex.ru>
2022-09-21 23:51:36 +0300

    Update test.py
    добавлены новые ошибки

--- test.py
+++ test.py
@@ -12,10 +12,12 @@ r = (requests.get(url))
 soup= BS(r.text, "lxml")
 
 link_a=("https://yandex.ru" + soup.find('div', class_="serp-item__preview").find('a', class_="serp-item__link").get('href'))
-#print(link_a)
+#print(link_a,"\n\n\n\n\n\n")
 url_img=str(link_a)
+
 r_img=requests.get(url_img)
+
 soupimg=BS(r_img.text,"lxml")
-#print(soupimg)
-img=(soupimg.find('div',class_="SwipeImage MMImageWrapper").find('img',class_="MMImage-Origin").get('src'))
+#print(soupimg) 
+img=(soupimg.find('div',class_="MMImageContainer").find('img',class_="MMImage-Origin").get('src'))  #АААААААААААа
 print(img)
