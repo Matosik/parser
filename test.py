ac31795c1246a24c60e7f13b1c237aec3d833faf
Matosik <boga4iov.matvei2016@yandex.ru>
2022-09-21 22:14:14 +0300

    Ошибка

--- test.py
+++ test.py
@@ -1,6 +1,21 @@
 import requests
 from bs4 import BeautifulSoup as BS
 
-url = 'https://yandex.ru/images/search?text=собака'
+def save(link):
+    file= link.split("/")[-1]
+    print(file)
+    pass
 
+
+url = "https://yandex.ru/images/search?text=собаки"
 r = (requests.get(url))
+soup= BS(r.text, "lxml")
+
+link_a=("https://yandex.ru" + soup.find('div', class_="serp-item__preview").find('a', class_="serp-item__link").get('href'))
+#print(link_a)
+url_img=str(link_a)
+r_img=requests.get(url_img)
+soupimg=BS(r_img.text,"lxml")
+#print(soupimg)
+img=(soupimg.find('div',class_="SwipeImage MMImageWrapper").find('img',class_="MMImage-Origin").get('src'))
+print(img)
