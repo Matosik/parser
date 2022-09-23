6ec28910ee736dcec26f3a9f761e67a91dc86d4e
Matosik <boga4iov.matvei2016@yandex.ru>
2022-09-23 22:50:11 +0300

    Beta Version 1.0
    Парсер готов работает исправно, но не на большое кол-во изображений
    Обновление:
    -программой можно пользоваться
    - исправлены ошибки(пока что всё еще парсит миниатюрки)
    - нельзя парсить много картинок
    все ошибки и недочеты и ограничения вскоре будут исправлены.

--- item.py
+++ item.py
@@ -3,18 +3,25 @@ from bs4 import BeautifulSoup as BS
 import os
 import csv
 
-diry= str(input("Input name directory: "))
+id=0
+def items_pars(counter, id, obj,directory ):
+    url=f"https://yandex.ru/images/search?text={obj}"
+    r = (requests.get(url))
+    soup= BS(r.text, "lxml")
+    img_url=soup.findAll('img',class_="serp-item__thumb justifier__thumb")
+    for i in range(counter):
+        img="https:"+ img_url[i].get("src")
+        img_d=requests.get(img).content
+        with open(f'C://Users/79093/Desktop/{directory}/{id}.jpg', "wb") as file:
+            file.write(img_d)
+            id+=1
+    return id
+
+diry= input("Input name directory: ")
+amount_dog= int(input("Input amount dog images: "))
+amount_cat=40-amount_dog
 os.mkdir(f"C://Users/79093/Desktop/{diry}")
-url = "https://yandex.ru/images/search?text=собаки"
-r = (requests.get(url))
-soup= BS(r.text, "lxml")
-img_url="https:"+soup.find('img',class_="serp-item__thumb justifier__thumb").get("src")
-print(img_url)
-img_d = requests.get(img_url).content
-with open(f'C://Users/79093/Desktop/{diry}/0.jpg','wb') as file:
-    file.write(img_d)
-
-
-#img_op = open('0' + '.jpg', 'wb')
-#img_op.write(img.content)
-#img_op.close()
+dogs="собаки"
+cats="кошки"
+id=items_pars(amount_dog,id,dogs,diry)
+items_pars(amount_cat,id,cats,diry)
